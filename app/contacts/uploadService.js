d715b420f581313c0bc5392fb264dafb225efedb
unknown <mobile.dev4@intel-i7-4.hq.elementalcrafts.com>
2015-12-28 18:33:00 +0530

    Cleaned up old code in branch

--- app/contacts/uploadService.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by dwellarkaruna on 28/10/15.
- */
-angular.module('contacts')
-    .service('uploadService', ['$http', 'appConstants', function ($http, appConstants) {
-        this.postDataAfterUpload = function (dataToPost) {
-           return $http.post(appConstants.APIBaseURL + 'MediaElement/Create', dataToPost)
-        }
-    }]);
