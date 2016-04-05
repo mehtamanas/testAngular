546720a2fc4e969380f7ba361c335a3a3876f293
unknown <Raman.Pratap@Intel-I7-19.hq.elementalcrafts.com>
2016-04-05 10:06:24 +0530

    tags tooltip

--- WPR2EI/contacts/contactsController.js
+++ WPR2EI/contacts/contactsController.js
@@ -424,7 +424,7 @@ angular.module('contacts')
                         duration: 100
                     }
                 },
-                filter: "td:nth-child(12)", //this filter selects the  column cells
+                filter: "td:nth-child(10)", //this filter selects the  column cells
                 position: "top",
                 beforeShow: function (e) {
                     console.log(e);
