3ab448a2244c40da9f7f3096a028929897d8886f
Ankit Dwivedi <ankit.dwivedi@dwellar.com>
2016-04-06 16:22:38 +0530

    Keep Me logged In

--- WPR2EI/app.js
+++ WPR2EI/app.js
@@ -71,15 +71,20 @@ angular.module('MainApp', [
         cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Loading...</div>';
         //cfpLoadingBarProvider.includeBar = false;
     }])
-.run(function ($rootScope, $location, $cookieStore, Idle, $window) {
+
+.run(function ($rootScope, $state, $cookieStore, Idle, $window) {
     $rootScope.$on("$stateChangeStart", function (event, next, current) {
         //if ($location.protocol() !== 'https') { //redirect http to https
         //  $window.location.href = $location.absUrl().replace('http', 'https');
         //}
+
         if (($cookieStore.get('userId') === undefined)) {
             if ((next.name).indexOf("app") > -1) {
-                $location.url('/app/index.html#/login');
+                $state.go('login');
             }
+        }
+        else {
+            $state.go('app.my_day');
         }
     })
 });
