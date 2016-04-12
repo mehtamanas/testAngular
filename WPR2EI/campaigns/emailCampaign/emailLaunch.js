03c1bf9681a17f8fb602c8ef98753dd6c1f4337b
unknown <abhishek.pingle@intel-i7-27.hq.elementalcrafts.com>
2016-04-12 18:38:53 +0530

    Email Relaunch Changes

--- WPR2EI/campaigns/emailCampaign/emailLaunch.js
+++ WPR2EI/campaigns/emailCampaign/emailLaunch.js
@@ -3,6 +3,7 @@
 var emialLaunchController = function ($scope, $state, $modalInstance,$cookieStore, apiService, $modal, $rootScope) {
     console.log("emialLaunchController");
     $scope.showcontent = true;
+    
 
     Url = "CampaignEmailTemplate/GetPeoplelistInCampaign/" + $cookieStore.get('campaign_ID');
 
@@ -32,6 +33,20 @@ function (error) {
         $modalInstance.dismiss('cancel');
     };
 
+    $scope.selectcheckbox = function ()
+    {
+        if ($scope.showcontent == true) {
+            $scope.params.whom_to_send = "NewpeopleAdded";
+        }
+        else {
+            $scope.params.whom_to_send = "ALL";
+        }
+    }
+    if ($scope.params.whom_to_send == undefined)
+    {
+        $scope.params.whom_to_send = "NewpeopleAdded";
+    }
+    
     $scope.launchCampaign = function ()
     {
         
@@ -40,12 +55,14 @@ function (error) {
             user_id:$cookieStore.get('userId'),
             organization_id: $cookieStore.get('orgID'),
             start_date : $scope.params.start_date,
-            whom_to_send: "All",
-            status: "Running",
+            whom_to_send: $scope.params.whom_to_send,
+            
         }
     apiService.post("CampaignEvent/Edit", postData).then(function (response) {
         var loginSession = response.data;
         $scope.cancel();
+        $scope.openSucessfullPopup();
+        $modalInstance.dismiss();
         $rootScope.$broadcast('REFRESH', 'projectGrid');
         },
           function (error) {
@@ -57,5 +74,14 @@ function (error) {
           });
     }
    
-
+    $scope.openSucessfullPopup = function () {
+        var modalInstance = $modal.open({
+            animation: true,
+            templateUrl: 'newuser/sucessfull.tpl.html',
+            backdrop: 'static',
+            controller: sucessfullController,
+            size: 'sm',
+            resolve: { items: { title: "Email Relaunced" } }
+        });
+    }
 }
