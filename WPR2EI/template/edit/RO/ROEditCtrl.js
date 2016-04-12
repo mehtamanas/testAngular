1d6e0f700a8b26b64b4d0e5a5b424969d3d92385
unknown <rupa.margaj@INTEL-I7-16.hq.elementalcrafts.com>
2016-04-12 18:15:55 +0530

    blogs

--- WPR2EI/template/edit/RO/ROEditCtrl.js
+++ WPR2EI/template/edit/RO/ROEditCtrl.js
@@ -1,17 +1,15 @@
-﻿ROCreateCtrl = function ($scope, $state, $cookieStore, apiService, $modalInstance, $rootScope, $sanitize, $modal, FileUploader) {
-    console.log('ROCreateCtrl');
+﻿ROEditCtrl = function ($scope, $state, $cookieStore, apiService, $modalInstance, $rootScope, $sanitize, $modal, emailData, FileUploader) {
     $scope.showTemplate = true;
     $scope.showPreview = false;
     $scope.params = {}
 
     $scope.params.orgId = $cookieStore.get('orgID');
     $scope.params.userid = $cookieStore.get('userId')
-    $scope.params.templateName;
-    $scope.params.subject;
+    $scope.params.templateName = emailData.template_name;
+    $scope.params.subject = emailData.subject;
     $scope.params.projectSelected;
     //$scope.params.orightml = '<h2>Try me!</h2><p>textAngular is a super cool WYSIWYG Text Editor directive for AngularJS</p><p><b>Features:</b></p><ol><li>Automatic Seamless Two-Way-Binding</li><li>Super Easy <b>Theming</b> Options</li><li style="color: green;">Simple Editor Instance Creation</li><li>Safely Parses Html for Custom Toolbar Icons</li><li class="text-danger">Doesn&apos;t Use an iFrame</li><li>Works with Firefox, Chrome, and IE8+</li></ol><p><b>Code at GitHub:</b> <a href="https://github.com/fraywing/textAngular">Here</a> </p>';
-    //$scope.params.htmlcontent = $scope.params.orightml;
-
+    $scope.params.htmlcontent = emailData.description;
     var uploader = $scope.uploader = new FileUploader({
         url: apiService.uploadURL,
     });
@@ -118,11 +116,12 @@
                 organization_id: $cookieStore.get('orgID'),
                 user_id: $cookieStore.get('userId'),
                 project_id: $scope.params.projectSelected,
-                document_type_id: "b987a90f-197e-467d-bf5d-1f0ddad58627"
+                document_type_id: "b987a90f-197e-467d-bf5d-1f0ddad58627",
+                id: emailData.id
             };
 
-            console.log($scope.params.htmlcontent);
-            apiService.post("Template/Create", postdata).then(function (response) {
+            
+            apiService.post("Template/EditTemplate", postdata).then(function (response) {
                 data = response.data[0];
                 $scope.openSucessfullPopup();
                 $scope.cancel();
@@ -134,25 +133,6 @@
             });
         }
     }
-
-
-    Url = "project/Get/" + $cookieStore.get('orgID');;
-    apiService.get(Url).then(function (response) {
-        $scope.projects = response.data;
-    },
-   function (error) {
-       if (error.status === 400)
-           alert(error.data.Message);
-       else
-           alert("Network issue");
-   });
-
-    $scope.selectproject = function ()
-    {
-        $scope.params.project_id = $scope.params.projectSelected;
-    };
-
-
 
     $scope.preview=function(){
         $scope.showTemplate = false;
@@ -171,7 +151,7 @@
     $scope.openSucessfullPopup = function () {
         var modalInstance = $modal.open({
             animation: true,
-            templateUrl: 'newuser/sucessfull.tpl.html',
+            templateUrl: 'newuser/Edited.tpl.html',
             backdrop: 'static',
             controller: sucessfullController,
             size: 'sm',
@@ -179,58 +159,6 @@
         });
     }
 
-  
 
-    $scope.generatePdf = function () {
-        demoFromHTML();
-    }
-
-    var demoFromHTML=function(){
-        var pdf = new jsPDF('p', 'pt', 'letter')
-
-        // source can be HTML-formatted string, or a reference
-        // to an actual DOM element from which the text will be scraped.
-        , source = $scope.params.htmlcontent
-
-        // we support special element handlers. Register them with jQuery-style 
-        // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-        // There is no support for any other type of selectors 
-        // (class, of compound) at this time.
-        , specialElementHandlers = {
-            // element with id of "bypass" - jQuery style selector
-            '#bypassme': function (element, renderer) {
-                // true = "handled elsewhere, bypass text extraction"
-                return true
-            }
-        }
-
-        margins = {
-            top: 80,
-            bottom: 60,
-            left: 40,
-            width: 522
-        };
-        // all coords and widths are in jsPDF instance's declared units
-        // 'inches' in this case
-        pdf.fromHTML(
-            source // HTML string or DOM elem ref.
-            , margins.left // x coord
-            , margins.top // y coord
-            , {
-                'width': margins.width // max width of content on PDF
-                , 'elementHandlers': specialElementHandlers
-            },
-            function (dispose) {
-                // dispose: object with X, Y of the last line add to the PDF 
-                //          this allow the insertion of new lines after html
-                //pdf.save('Test.pdf');
-                var string = pdf.output('datauristring');
-                pdf.save('Test.pdf');
-                window.open(string);
-                //pdf.output('datauri');
-            },
-            margins
-        )
-    }
-  
+    
 };
