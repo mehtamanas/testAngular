e82ac72afd089545b3de270ba839938de728c179
unknown <dattatraya.revagade@Intel-I7-10.hq.elementalcrafts.com>
2016-04-20 11:22:40 +0530

    task.js postpone task code

--- WPR2EI/task/task.js
+++ WPR2EI/task/task.js
@@ -201,7 +201,35 @@
                  "style": "text-align:center;cursor:pointer"
              }
 
+           }, {
+               title: "postpone",
+               template: '#if (status!="Completed") {# <a class="btn btn-primary" id="postpone_now" ng-click="openPostpone(dataItem)">Postpone</a> #}#',
+               attributes:
+             {
+                 "style": "text-align:center"
+             }
+
            }, ]
+        };
+
+
+        $scope.openPostpone = function (d) {
+            $scope.taskID = d.task_id;
+            window.sessionStorage.selectedCustomerID = $scope.taskID;
+            $cookieStore.put('company_name', d.company_name);
+            $cookieStore.put('contactID', d.contact_id);
+            $cookieStore.put('lead_name', d.Contact_name);
+            $cookieStore.put('task_name', d.name);
+            $cookieStore.put('taskID', $scope.taskID);
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'contacts/postponed/taskpostponed.html',
+                backdrop: 'static',
+                controller: postponedController,
+                size: 'lg'
+
+            });
+
         };
 
         //$scope.openAddtaskPopup = function () {
