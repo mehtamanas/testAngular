b568c7c56b5d92173688a4cec541bb60095f92e1
unknown <rupa.margaj@INTEL-I7-16.hq.elementalcrafts.com>
2016-04-22 16:58:35 +0530

    create Unsubscribed list

---
+++ WPR2EI/unsubscribe/module.js
@@ -0,0 +1,12 @@
+﻿angular.module('unsubscribe', [])
+
+    .config(['$stateProvider', function ($stateProvider) {
+        $stateProvider
+            .state('app.unsubscribe', {
+                url: '/unsubscribe',
+                templateUrl: 'unsubscribe/unsubscribe.html',
+                controller: 'unsubscribeController',
+
+            })
+
+    }]);
