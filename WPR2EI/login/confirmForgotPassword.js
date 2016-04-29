4a42207b04ebd8882889f83120580231a3e50b6f
unknown <pradip.patil@INTEL-I7-32.hq.elementalcrafts.com>
2016-04-29 14:12:36 +0530

    forgot password new design added

---
+++ WPR2EI/login/confirmForgotPassword.js
@@ -0,0 +1,22 @@
+﻿var confirmResetPassword = function ($scope, $state, $cookieStore, apiService, $modalInstance) {
+    console.log('confirmResetPassword');
+
+    $scope.resetAccountEmail = $cookieStore.get('AccountEmail');
+ 
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+    $scope.reset = function () {
+        $scope.params = {};
+    }
+
+
+
+    //end
+
+
+ 
+
+};
